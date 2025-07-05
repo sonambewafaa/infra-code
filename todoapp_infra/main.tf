24d6728c916f4c02ec677cc1780f24bef6ed8458
bhavishay <bhavishay111@gmail.com>
2025-07-05 01:03:45 -0400

    rg name and location updated

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -6,8 +6,8 @@ module "resource_group" {
 
 module "resource_group1" {
   source = "../modules/azurerm_resource_group"
-  resource_group_name = "101-rg-canada"
-  resource_group_location = "centralindia"
+  resource_group_name = "rg-canada"
+  resource_group_location = "canada central"
 }
 
 module "virtual_network" {
